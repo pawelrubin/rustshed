5e648f10be109694894ce5b6f7eef708139215c5
Paweł Rubin <pawelrubindev@gmail.com>
2022-06-16 15:40:17 +0200

    feat: Add Option type.

---
+++ rustshed/list.py
@@ -0,0 +1,13 @@
+from typing import SupportsIndex, TypeVar
+
+from rustshed.option import Null, Option, Some
+
+T = TypeVar("T")
+
+
+class SafeList(list[T]):
+    def get(self, index: SupportsIndex) -> Option[T]:
+        try:
+            return Some(self[index])
+        except IndexError:
+            return Null
